40129088815d83674cf8fa92316457012731d4ad
ParasP41 <parasvishwakarma198@gmail.com>
2025-01-26 15:33:08 +0530

    EMS

---
+++ src/Components/DashBoard/AdminDashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from '../Other/Header'
+export default function AdminDashboard() {
+  return (
+    <div className='bg-black h-screen w-full p-10 ' >
+        <Header />
+    </div>
+  )
+}
